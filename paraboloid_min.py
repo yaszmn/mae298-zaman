0ca74e603818c9c59eba76fc1e532050ce4ba5f3
yaszmn <yasminxzaman@gmail.com>
2025-10-08 15:14:15 -0700

    AVIARY! OTHER THINGS!

--- paraboloid_min.py
+++ paraboloid_min.py
@@ -1,9 +1,21 @@
 import openmdao.api as om
+import numpy as np
 
 # build the model
 prob = om.Problem()
 
-prob.model.add_subsystem('paraboloid', om.ExecComp('f = (x-3)**2 + x*y + (y+4)**2 - 3'))
+class Paraboloid(om.ExplicitComponent):
+	def setup(self):
+		self.add_input('x', val=0.0)
+		self.add_input('y', val=0.0)
+		self.add_output('f', val=0.0)
+
+	def compute(self, inputs, outputs):
+		x = inputs['x']
+		y = inputs['y']
+		outputs['f'] = (x - 4)**2 + x * y + (y + 3)**2 - 3
+
+prob.model.add_subsystem('paraboloid', Paraboloid())
 
 # setup the optimization
 prob.driver = om.ScipyOptimizeDriver()
@@ -16,8 +28,8 @@ prob.model.add_objective('paraboloid.f')
 prob.setup()
 
 # Set initial values.
-prob.set_val('paraboloid.x', 3.0)
-prob.set_val('paraboloid.y', -4.0)
+prob.set_val('paraboloid.x', -1.0)
+prob.set_val('paraboloid.y', 7.0)
 
 # run the optimization
 prob.run_driver();
